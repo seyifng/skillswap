e96adb4872ee5669679c68ec637b8a327871a0e8
Robert Bray <rbray77777@gmail.com>
2026-01-30 15:16:59 -0500

    got the backend setup

---
+++ ss-backend/src/main/java/com/skillswap/ss_backend/SsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skillswap.ss_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsBackendApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SsBackendApplication.class, args);
+	}
+
+}
---
+++ ss-backend/src/test/java/com/skillswap/ss_backend/SsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillswap.ss_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
